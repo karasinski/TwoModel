f8e436c847ff3423a3ddc4f30ab7c602f6c1d30c
John Karasinski <karasinski@gmail.com>
2017-12-07 10:22:49 -0800

    add adjustable dynamics gain; set up trackers to track correct inputs; ignore log folder

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -8,7 +8,7 @@ public class Controller : MonoBehaviour
 {
     public GameObject Cursor, Target, Text;
     public int TotalTrials, CurrentTrial, LengthOfTrial, SystemOrder;
-    public double DisturbanceGain, InputGain;
+    public double K, DisturbanceGain, InputGain;
 
     private StreamWriter Writer;
     private String Filename;
@@ -19,15 +19,18 @@ public class Controller : MonoBehaviour
 
     public class Tracking
     {
-        public int SystemOrder;
-        public Matrix<double> A, B, C, D, y, x, x0, u, u0, InputGainMatrix;
-        public double Disturbance, DisturbanceGain, InputGain;
+        private int SystemOrder;
+        private Matrix<double> A, B, C, D, y, x, x0, u, u0, InputGainMatrix;
+        private double K, Disturbance, DisturbanceGain, InputGain;
+        private string Axis;
 
-        public Tracking(int SystemOrder, double DisturbanceGain, double InputGain)
+        public Tracking(string Axis, double K, int SystemOrder, double DisturbanceGain, double InputGain)
         {
+            this.K = K;
             this.SystemOrder = SystemOrder;
             this.DisturbanceGain = DisturbanceGain;
             this.InputGain = InputGain;
+            this.Axis = Axis;
             Initialize();
         }
 
@@ -40,37 +43,22 @@ public class Controller : MonoBehaviour
                     A = Matrix<double>.Build.DenseOfArray(new double[,] { { 0 } });
                     B = Matrix<double>.Build.DenseOfArray(new double[,] { { 0 } });
                     C = Matrix<double>.Build.DenseOfArray(new double[,] { { 0 } });
-                    D = Matrix<double>.Build.DenseOfArray(new double[,] { { 5 } });
+                    D = Matrix<double>.Build.DenseOfArray(new double[,] { { K } });
                     x = Matrix<double>.Build.DenseOfArray(new double[,] { { 0 } });
                     break;
                 case 1:
-                    A = Matrix<double>.Build.DenseOfArray(new double[,] { { -0 } });
+                    A = Matrix<double>.Build.DenseOfArray(new double[,] { { 0 } });
                     B = Matrix<double>.Build.DenseOfArray(new double[,] { { 1 } });
-                    C = Matrix<double>.Build.DenseOfArray(new double[,] { { 5 } });
+                    C = Matrix<double>.Build.DenseOfArray(new double[,] { { K } });
                     D = Matrix<double>.Build.DenseOfArray(new double[,] { { 1 } });
                     x = Matrix<double>.Build.DenseOfArray(new double[,] { { 0 } });
                     break;
                 case 2:
-                    A = Matrix<double>.Build.DenseOfArray(new double[,] {
-                        {-0, -0 },
-                        { 1, 0 }
-                    });
-
-                    B = Matrix<double>.Build.DenseOfArray(new double[,] {
-                        { 1 },
-                        { 0 }
-                    });
-
-                    C = Matrix<double>.Build.DenseOfArray(new double[,] {
-                        { 0, 5 }
-                    });
-
+                    A = Matrix<double>.Build.DenseOfArray(new double[,] { { 0, 0 }, { 1, 0 } });
+                    B = Matrix<double>.Build.DenseOfArray(new double[,] { { 1 }, { 0 } });
+                    C = Matrix<double>.Build.DenseOfArray(new double[,] { { 0, K } });
                     D = Matrix<double>.Build.DenseOfArray(new double[,] { { 0 } });
-
-                    x = Matrix<double>.Build.DenseOfArray(new double[,] {
-                        { 0 },
-                        { 0 }
-                    });
+                    x = Matrix<double>.Build.DenseOfArray(new double[,] { { 0 }, { 0 } });
                     break;
             };
 
@@ -87,7 +75,7 @@ public class Controller : MonoBehaviour
         {
             Disturbance = DisturbanceGain * generateDisturbance(CurrentTime);
 
-            u = InputGainMatrix * Input.GetAxis("Vertical");
+            u = InputGainMatrix * Input.GetAxis(Axis);
 
             x += (A * x + B * u) * Time.deltaTime;
             y = (C * x + D * u) + Disturbance;
@@ -114,17 +102,20 @@ public class Controller : MonoBehaviour
             return track;
         }
 
-        public float Output() {
+        public float Output()
+        {
             return (float)y[0, 0];
         }
 
-        public void Reset() {
+        public void Reset()
+        {
             x = x0;
             u = u0;
         }
 
 
-        private string Stringify(Matrix<double> input) {
+        private string Stringify(Matrix<double> input)
+        {
             var str = "";
             foreach (double element in input.ToArray())
             {
@@ -146,8 +137,8 @@ public class Controller : MonoBehaviour
     void Start()
     {
         // Initialize a 1D tracker
-        Trackers[0] = new Tracking(SystemOrder, DisturbanceGain, InputGain);
-        Trackers[1] = new Tracking(SystemOrder, DisturbanceGain, InputGain);
+        Trackers[0] = new Tracking("Horizontal", K, SystemOrder, DisturbanceGain, InputGain);
+        Trackers[1] = new Tracking("Vertical", K, SystemOrder, DisturbanceGain, InputGain);
     }
 
     void Update()
@@ -217,10 +208,6 @@ public class Controller : MonoBehaviour
         {
             // Exit experiment
             Text.SetActive(true);
-        }
-        else
-        {
-            // Something to begin next trial
         }
 
         RunningTrial = false;
